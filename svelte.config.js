1e8efa223586aa95fd9696e8f2d225b989b5bd98
johannes <johannes@artificialmuseum.com>
2025-07-28 16:21:07 +0200

    undefined

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ out: "build" }),
-	}
+	},
+	preprocess: [vitePreprocess({})],
 };
 
 export default config;
